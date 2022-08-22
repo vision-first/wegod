5dc2f6b1b56d48cadfef4d8f7ddd191ecdecffd1
yangzhouhong <yangzhouhong@example.com>
2022-08-22 14:51:34 +0800

    ok

--- internal/pkg/api/apis/user.go
+++ internal/pkg/api/apis/user.go
@@ -2,8 +2,11 @@ package apis
 
 import (
 	"github.com/995933447/log-go"
+	"github.com/995933447/reflectutil"
 	"github.com/vision-first/wegod/internal/pkg/api"
 	"github.com/vision-first/wegod/internal/pkg/api/dtos"
+	"github.com/vision-first/wegod/internal/pkg/facades"
+	"github.com/vision-first/wegod/internal/pkg/services"
 )
 
 type User struct {
@@ -19,7 +22,23 @@ func NewUser(logger *log.Logger) *User {
 func (u *User) GetUserInfo(ctx api.Context, req *dtos.GetUserInfoReq) (*dtos.GetUserInfoResp, error) {
     var resp dtos.GetUserInfoResp
 
-    // TODO.write your logic
+   	authIdent, err := ctx.GetAuthIdentOrFailed()
+	if err != nil {
+		u.logger.Error(ctx, err)
+		return nil, err
+	}
+
+	userDO, err := services.NewUser(u.logger).GetUserById(ctx, authIdent.GetUserId())
+	if err != nil {
+		u.logger.Error(ctx, err)
+		return nil, err
+	}
+
+	err = reflectutil.CopySameFields(userDO, &resp)
+	if err != nil {
+		u.logger.Error(ctx, err)
+		return nil, err
+	}
 
     return &resp, nil
 }
@@ -27,7 +46,29 @@ func (u *User) GetUserInfo(ctx api.Context, req *dtos.GetUserInfoReq) (*dtos.Get
 func (u *User) SetUserInfo(ctx api.Context, req *dtos.SetUserInfoReq) (*dtos.SetUserInfoResp, error) {
     var resp dtos.SetUserInfoResp
 
-    // TODO.write your logic
+	authIdent, err := ctx.GetAuthIdentOrFailed()
+	if err != nil {
+		u.logger.Error(ctx, err)
+		return nil, err
+	}
+
+	userDO, err := services.NewUser(u.logger).GetUserById(ctx, authIdent.GetUserId())
+	if err != nil {
+		u.logger.Error(ctx, err)
+		return nil, err
+	}
+
+	err = reflectutil.CopySameFields(req, userDO)
+	if err != nil {
+		u.logger.Error(ctx, err)
+		return nil, err
+	}
+
+	err = facades.MustGormDB(ctx, u.logger).Save(userDO).Error
+	if err != nil {
+		u.logger.Error(ctx, err)
+		return nil, err
+	}
 
     return &resp, nil
 }
